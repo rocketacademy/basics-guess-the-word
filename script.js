4f07ffe6369d25dae2ac6e61ea06bd7957eb21c2
alvinljt <alvinljt@gmail.com>
2020-09-01 11:12:58 +0800

    included easy and hard modes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // declare and initialise the secret word
-var secretWord = ['c', 'a', 't'];
+var secretWord = ['p', 'i', 'z', 'z', 'a'];
 
 var secretWordLength = secretWord.length;
+// make a copy of secret word for use in hard mode
+var copyOfSecretWord = ['p', 'i', 'z', 'z', 'a'];
 
 // array to store guessed letters
 var guessedLetters = [];
@@ -17,53 +19,77 @@ var completedFigure = ['(', '凸', 'ಠ', '益', 'ಠ', ')', '凸'];
 
 var figure = '';
 var numOfGuessesLeft = 7;
+var mode = 'choosing game difficulty';
+var myOutputValue = '';
 
 // play the game
 var main = function (input) {
-  var index = 0;
-  var myOutputValue = '';
-  var guessResult = 'You guessed wrong!';
+  if (mode == 'choosing game difficulty') {
+    if (input == 'hard' || input == 'easy') {
+      mode = input;
+      myOutputValue = 'You have chosen ' + mode + ' mode.<br>Please key in a letter to start guessing the word.';
+    } else {
+      myOutputValue = 'You have entered an invalid mode.<br>Please key in \'hard\' or \'easy\'';
+    }
+  } else {
+    var index = 0;
+    var guessResult = 'You guessed wrong!';
 
-  guessedLetters.push(input);
+    guessedLetters.push(input);
 
-  while (index < secretWordLength) {
-    var currentLetter = secretWord[index];
+    while (index < secretWordLength) {
+      console.log('checking letter at index: ' + index);
+      var currentLetter = secretWord[index];
 
-    if (input == currentLetter) {
+      if (input == currentLetter) {
       // update word formed using correct guesses
-      wordFormed[index] = secretWord[index];
+        wordFormed[index] = secretWord[index];
 
-      guessResult = 'You guessed correct!';
+        guessResult = 'You guessed correct!';
+
+        // stop matching letters if a letter has already been found once in current guess.
+        if (mode == 'hard') {
+          if (input == copyOfSecretWord[index]) {
+            copyOfSecretWord[index] = '_';
+            index = secretWordLength;
+          }
+        }
+      }
+      index += 1;
+      console.log('is input == currentLetter: ' + (input == currentLetter));
     }
-    index += 1;
-  }
+    console.log('copy of secret word: ' + copyOfSecretWord);
+    console.log('secret word is: ' + secretWord);
+    console.log('guess result: ' + guessResult);
 
-  // add character to figure if user guessed wrong
-  if (guessResult == 'You guessed wrong!') {
-    figure += completedFigure[7 - numOfGuessesLeft];
+    // add character to figure if user guessed wrong
+    if (guessResult == 'You guessed wrong!') {
+      figure += completedFigure[7 - numOfGuessesLeft];
 
-    numOfGuessesLeft = Number(numOfGuessesLeft) - 1;
-  }
-
-  // Check if user guessed all the letters and tell them that they won
-  var result = secretWord.every((val, arrayIndex) => val === wordFormed[arrayIndex]);
-  console.log('user guessed all letters? ' + result);
-
-  if (result == true) {
-    var answer = '';
-
-    for (let i = 0; i < secretWordLength; i += 1) {
-      answer += secretWord[i];
+      numOfGuessesLeft = Number(numOfGuessesLeft) - 1;
     }
 
-    return 'You have guessed the word!<br>The word is ' + answer + '.';
+    myOutputValue = 'Guess the word:<br>' + wordFormed + '<br>' + guessResult + '<br>Number of wrong guesses left = ' + numOfGuessesLeft + '<br>Guessed letters: ' + guessedLetters;
+
+    // Check if user guessed all the letters and tell them that they won
+    var result = secretWord.every((val, arrayIndex) => val === wordFormed[arrayIndex]);
+    console.log('user guessed all letters? ' + result);
+
+    if (result == true) {
+      var answer = '';
+
+      for (let i = 0; i < secretWordLength; i += 1) {
+        answer += secretWord[i];
+      }
+
+      return 'You have guessed the word!<br>The word is ' + answer + '.';
+    }
+
+    // stops the game if the user has formed the figure aka used all 7 wrong guesses
+    if (numOfGuessesLeft == 0) {
+      return figure + '<br>You lost since you have used up all 7 wrong guesses 🙁.<br>Please refresh the page to try again.';
+    }
   }
 
-  // stops the game if the user has formed the figure aka used all 7 wrong guesses
-  if (numOfGuessesLeft == 0) {
-    return figure + '<br>You lost since you have used up all 7 wrong guesses 🙁.<br>Please refresh the page to try again.';
-  }
-
-  myOutputValue = wordFormed + '<br>' + guessResult + '<br>Number of wrong guesses left = ' + numOfGuessesLeft + '<br>Guessed letters: ' + guessedLetters;
   return myOutputValue;
 };
