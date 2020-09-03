16d88a08691becb668cce9f28426dfa6628cae85
pinnlawjindakul <pachara.lawjindakul@gmail.com>
2020-09-03 11:49:07 +0800

    added word matching part

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-var main = function(input) {
-  var myOutputValue = 'hello world';
+var main = function (input) {
+
+  var word = ['c', 'a', 't'];
+  var remainingLetters = word.length;
+  var answerArray = [];
+
+  // if correct, add to the answerArray
+  for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+  };
+  for (var j = 0; j < word.length; j++) {
+    if (word[j] == input) {
+      answerArray[j] = input;
+      remainingLetters--;
+    }
+  };
+
+  // if wrong, add to the hang man
+  var hangman = [];
+
+  var myOutputValue = answerArray + "Hangman:" + ;
   return myOutputValue;
 };
+
+
+
+
+
